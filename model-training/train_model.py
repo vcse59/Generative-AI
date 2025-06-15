56576c18537e72573f977029603b8c2cfcab0404
Vivek Yadav <v.cse59@gmail.com>
2025-06-15 12:17:37 -0700

    Update in test_image file

--- model-training/train_model.py
+++ model-training/train_model.py
@@ -76,7 +76,7 @@ import numpy as np
 import os
 
 # Load and preprocess single image
-img = image.load_img('test_image.jpg', target_size=(150, 150))
+img = image.load_img(os.path.join('model-training', 'test_image.jpg'), target_size=(150, 150))
 img_array = image.img_to_array(img) / 255.0
 img_array = np.expand_dims(img_array, axis=0)
 
